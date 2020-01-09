0d29c03e1edd80a60a5a0724c6901d7b9a0d8c8f
mehdi <lockee14@msn.com>
2020-01-09 12:36:23 +0900

    create a trigger onOpen

--- Code.js
+++ Code.js
@@ -1,3 +1,18 @@
-function myFunction() {
-  
+/**
+ * A special function that runs when the spreadsheet is first
+ * opened or reloaded. onOpen() is usually used to add custom
+ * menu items to the spreadsheet.
+ */
+function onOpen() {
+    const ui = SpreadsheetApp.getUi();
+    ui.createMenu('custom menu')
+        .addItem('fill with name', 'getName')
+        .addToUi();
+}
+
+/**
+ * get some name from an api and put them in the spreadsheet
+ */
+function getName() {
+    Logger.log("the function got called");
 }
